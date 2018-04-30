167ae66a04b0ea6afa626d6d67fec1c3e2b65192
Andres Aguilar <andresyoshimar@gmail.com>
2018-04-30 11:08:17 -0500

    Agregando animacion de entrada de las listas de pendientes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,22 @@
 import {Component, OnInit} from '@angular/core';
 import {ListService} from '../services/lists.service';
+import {animate, trigger, state, stagger, style, transition, query} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.component.html'
+  templateUrl: 'home.component.html',
+  animations: [
+    trigger('enterState',[
+      transition('* => *', [
+        query(':enter', [
+          style({transform: 'translateX(-100%)', opacity: 0}),
+          stagger(100, [
+            animate(300, style({transform: 'translateX(0)', opacity: 1}))
+          ])
+        ], {optional: true})
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
   public message: string;
